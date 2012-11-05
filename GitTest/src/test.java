8f3193e8fd06ee479ec874eedeedd5e7d3190085
uemon <uemon1337@googlemail.com>
2012-11-05 11:09:00 +0100

    Erste Änderung.

--- GitTest/src/test.java
+++ GitTest/src/test.java
@@ -1,14 +1,27 @@
+import java.io.*;
 
 public class test {
 
     /**
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         // TODO Auto-generated method stub
 
-        System.out.println("Das ist ein Test.");
+        String nameAusgabedatei; 
+        File ausgabedatei; 
+        FileWriter fw; 
+        BufferedWriter bw; 
+
+        
+          nameAusgabedatei = "testdatei"; 
+          ausgabedatei = new File(nameAusgabedatei+".html"); 
+          fw = new FileWriter(ausgabedatei); 
+          bw = new BufferedWriter(fw); 
+          bw.write("<html><h1>Hallo</h1></html>"); 
+          bw.close(); 
         
     }
-
 }
+
+
